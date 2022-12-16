9647a5e1047c6417fcafe5055b8da3903d10abba
Igor Pavlov <i.pavlov@uzum.com>
2022-12-16 12:49:36 +0300

    feat(deployment): preparing for publishing

--- mix.exs
+++ mix.exs
@@ -1,14 +1,18 @@
 defmodule TeslaDumper.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
   def project do
     [
       app: :tesla_dumper,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description(),
+      source_url: "https://github.com/beastia/tesla_dumper"
     ]
   end
 
@@ -21,12 +25,25 @@ defmodule TeslaDumper.MixProject do
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
+  defp package do
+    [
+      maintainers: ["Igor Pavlov"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/beastia/tesla_dumper"}
+    ]
+  end
+
+  defp description do
+    "Simple dumper of Tesla Env to a playable curl request"
+  end
+
   defp deps do
     [
       {:tesla, "~> 1.4"},
       {:mimic, "~> 1.7.4", only: :test},
       {:bypass, "~> 2.1.0", only: :test},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
